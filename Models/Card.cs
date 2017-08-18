1a336420bcdaee05584dc25f8b977fd3fc3f4219
Michael Hartley <mtjhartley@gmail.com>
2017-08-18 10:17:22 -0700

    added card class with CardId, Text, Color, and IsExposed attributes

--- Models/Card.cs
+++ Models/Card.cs
@@ -7,5 +7,9 @@ namespace codenames.Models
 {
     public class Card
     {
+        public int CardId {get;set;}
+        public string Text {get;set;}
+        public string Color {get;set;}
+        public Boolean IsExposed {get;set;}
     }
 }
